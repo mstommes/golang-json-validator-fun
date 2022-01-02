f22279aa69b96271481b72cc673c265abdc8c973
M. Stommes <mstommes@gmail.com>
2022-01-02 13:26:32 -0600

    GoLang Json Schema validator project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module mstommes.com/jsonschemavalidator
+
+go 1.16
+
+require (
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/gofiber/fiber/v2 v2.24.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Dog struct {
+    Name      string `json:"name" validate:"required,min=3,max=12"`
+    Age       int    `json:"age" validate:"required,numeric"`
+    IsGoodBoy bool   `json:"isGoodBoy" validate:"required"`
+}
+
+type IError struct {
+    Field string
+    Tag   string
+    Value string
+}
+
+var Validator = validator.New()
+
+func ValidateAddDog(c *fiber.Ctx) error {
+	var errors []*IError
+    body := new(Dog)
+    c.BodyParser(&body)
+
+	err := Validator.Struct(body)
+    if err != nil {
+        for _, err := range err.(validator.ValidationErrors) {
+            var el IError
+            el.Field = err.Field()
+            el.Tag = err.Tag()
+            el.Value = err.Param()
+            errors = append(errors, &el)
+        }
+        return c.Status(fiber.StatusBadRequest).JSON(errors)
+    }
+    return c.Next()
+}
+
+
+func main(){
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Everything working")
+	})
+
+	app.Post("/", ValidateAddDog, func(c *fiber.Ctx) error {
+        body := new(Dog)
+        c.BodyParser(&body)
+        return c.Status(fiber.StatusOK).JSON(body)
+    })
+
+	app.Listen(":3000")
+}
